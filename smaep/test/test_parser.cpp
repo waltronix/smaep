6addf8dfca6461f3bc59a756be946e0e25c47210
Andreas Walter <andreas.walter@waltronix.de>
2020-09-26 20:22:22 +0000

    code coverage with help of stable coder

--- smaep/test/test_parser.cpp
+++ smaep/test/test_parser.cpp
@@ -1,31 +1,33 @@
 #include <list>
 #include <string>
 
+#include <math.h>
+
 #include "catch2/catch.hpp"
 
+#include "i_data_source.h"
 #include "parser.hpp"
 
-// #include "data/map_source.h"
-// #include "sample_expressions.h"
-
-TEST_CASE("parse_to_double_ast", "parser") {
+TEST_CASE("parse_numbers", "parser") {
 
   auto expressions = std::list<std::tuple<std::string, double>>();
-  // expressions.push_back({"1", 1 });
-  expressions.push_back({"1 + 1", 2});
-  expressions.push_back({"1 * 1", 1});
-  expressions.push_back({"(1 * 1)", 1});
-  expressions.push_back({"2 * (3 + 1)", 8});
-  expressions.push_back({"2 * 3 + 1", 7});
-  expressions.push_back({"1 * 2 + 3", 5});
-  expressions.push_back({"1 + 2 * 3", 7});
-  expressions.push_back({"1 / 2", 0.5});
-  expressions.push_back({"1.5 / 2", 0.75});
-  expressions.push_back({"abs(-2)", 2});
-  expressions.push_back({"sin(1/3)^2 + cos(1/3)^2", 1});
+  expressions.push_back({"0", 0 });
+  expressions.push_back({"1", 1 });
+  expressions.push_back({"2", 2 });
+  expressions.push_back({"3", 3 });
+  expressions.push_back({"4", 4 });
+  expressions.push_back({"5", 5 });
+  expressions.push_back({"6", 6 });
+  expressions.push_back({"7", 7 });
+  expressions.push_back({"8", 8 });
+  expressions.push_back({"9", 9 });
 
-  // auto expressions = samples::get_double_samples();
-  // auto ms = smaep::data::map_source<double>::create_top_ten();
+  expressions.push_back({"-1", -1 });
+  expressions.push_back({"-5", -5 });
+
+  expressions.push_back({"10", 10 });
+  expressions.push_back({"222", 222 });
+  expressions.push_back({"-222", -222 });
 
   for (const auto& e : expressions) {
     auto problem = std::get<0>(e);
@@ -40,29 +42,104 @@ TEST_CASE("parse_to_double_ast", "parser") {
   }
 }
 
-// TEST_CASE("parse_to_int_ast", "parser") {
-//   auto expressions = samples::get_int64_samples();
-//   auto ms = smaep::data::map_source<int64_t>::create_top_ten();
+TEST_CASE("parsing fails", "parser") {
+  auto expressions = std::list<std::string>();
+  expressions.push_back("+");
 
-//   for (const auto& e : expressions) {
-//     auto problem = std::get<0>(e);
-//     auto expected = std::get<1>(e);
+  auto config = smaep::create_parser_config<double>();
 
-//     INFO(problem);
-//     auto ast = smaep::parse<int64_t>(problem);
-//     auto result = ast.value(ms);
+  for (const auto& problem : expressions) {
 
-//     CHECK(expected == result);
-//   }
-// }
+    INFO(problem);
+    CHECK_THROWS(smaep::parse<double>(problem, config));
+  }
+}
 
-TEST_CASE("add_operator", "parser") {
-  auto expressions =
-      std::list<std::pair<std::string, int64_t>>{{"1 + 1", 2}, {"11 ~ 10", 10}};
+TEST_CASE("primitive_operations", "parser") {
+
+  auto expressions = std::list<std::tuple<std::string, double>>();
+  expressions.push_back({"1 + 1", 2 });
+  expressions.push_back({"2 - 1", 1 });
+  expressions.push_back({"2 * 2", 4 });
+  expressions.push_back({"6 / 2", 3 });
+  expressions.push_back({"2^3", 8 });
+
+  for (const auto& e : expressions) {
+    auto problem = std::get<0>(e);
+    auto expected = std::get<1>(e);
+
+    INFO(problem);
+    auto ast = smaep::parse<double>(problem);
+    // auto result = ast.value(ms);
+    auto result = ast.value();
+
+    CHECK(expected == result);
+  }
+}
+
+TEST_CASE("function_operations", "parser") {
+
+  auto expressions = std::list<std::tuple<std::string, double>>();
+  expressions.push_back({"abs(2)", 2 });
+  expressions.push_back({"abs(-2)", 2 });
+
+  expressions.push_back({"exp(0)", 1 });
+  expressions.push_back({"exp(1)", exp(1.) });
+
+  expressions.push_back({"sin(0)", 0 });
+  expressions.push_back({"sin(30)", sin(30.) });
+
+  expressions.push_back({"cos(0)", 1 });
+  expressions.push_back({"cos(30)", cos(30.) });
+
+  expressions.push_back({"tan(0)", 0 });
+  expressions.push_back({"tan(30)", tan(30.) });
+
+  for (const auto& e : expressions) {
+    auto problem = std::get<0>(e);
+    auto expected = std::get<1>(e);
+
+    INFO(problem);
+    auto ast = smaep::parse<double>(problem);
+    // auto result = ast.value(ms);
+    auto result = ast.value();
+
+    CHECK(expected == result);
+  }
+}
+
+TEST_CASE("braced_expressions", "parser") {
+
+  auto expressions = std::list<std::tuple<std::string, double>>();
+  expressions.push_back({"(1 + 1) * 2", 4 });
+  expressions.push_back({"2 / (1 + 1)", 1 });
+  expressions.push_back({"sin(1/3)^2 + cos(1/3)^2", 1});
+
+  for (const auto& e : expressions) {
+    auto problem = std::get<0>(e);
+    auto expected = std::get<1>(e);
+
+    INFO(problem);
+    auto ast = smaep::parse<double>(problem);
+    // auto result = ast.value(ms);
+    auto result = ast.value();
+
+    CHECK(expected == result);
+  }
+}
+
+TEST_CASE("user_defined_operator", "parser") {
+  auto expressions = std::list<std::pair<std::string, int64_t>>();
+  expressions.push_back({"1 + 1", 2});
+  expressions.push_back({"11 ~ 10", 10});
+  expressions.push_back({"11 || 10", 1});
 
   auto config = smaep::create_parser_config<int64_t>();
   config->add_operator("~", smaep::order(1),
                        [](auto x, auto i) { return (x / i) * i; });
+
+  config->add_operator("||", smaep::order(1),
+                       [](auto x, auto i) { return 1; });
 
   for (const auto& e : expressions) {
     auto problem = std::get<0>(e);
@@ -71,6 +148,45 @@ TEST_CASE("add_operator", "parser") {
     INFO(problem);
     auto ast = smaep::parse<int64_t>(problem, config);
     auto result = ast.value();
+
+    CHECK(expected == result);
+  }
+}
+
+TEST_CASE("user_defined_function", "parser") {
+  auto expressions = std::list<std::pair<std::string, int64_t>>();
+  expressions.push_back({"my_func(1)", 5});
+  expressions.push_back({"my_func(3)", 5});
+
+  auto config = smaep::create_parser_config<int64_t>();
+  config->add_function("my_func", smaep::order(1),[](auto x) { return 5; });
+
+  for (const auto& e : expressions) {
+    auto problem = std::get<0>(e);
+    auto expected = std::get<1>(e);
+
+    INFO(problem);
+    auto ast = smaep::parse<int64_t>(problem, config);
+    auto result = ast.value();
+
+    CHECK(expected == result);
+  }
+}
+
+TEST_CASE("data_field", "parser") {
+  auto expressions = std::list<std::pair<std::string, int64_t>>();
+  expressions.push_back({"data[one]", 0});
+
+  auto config = smaep::create_parser_config<int64_t>();
+  auto empty_data = smaep::data::empty_source<int64_t>();
+
+  for (const auto& e : expressions) {
+    auto problem = std::get<0>(e);
+    auto expected = std::get<1>(e);
+
+    INFO(problem);
+    auto ast = smaep::parse<int64_t>(problem, config);
+    auto result = ast.value(empty_data);
 
     CHECK(expected == result);
   }
